1014224b0d3119f9a47580ab86028208633e0ab8
dinushchathurya <dinushchathurya21@gmail.com>
2022-04-27 20:23:14 +0530

    :sparkles: Create function to Create Product

--- CreateProducts/index.js
+++ CreateProducts/index.js
@@ -1,13 +1,44 @@
+const {insertEntity} = require('../service/tableService');
+
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    try {
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        if (!req.body) {
+            context.res = {
+                status: 400,
+                body: "Request body required",
+            };
+            return;
+        }
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+        const { product, title, content } = req.body;
+
+        if (!product || !title || !content) {
+            context.res = {
+                status: 400,
+                body: "Please pass product, title and content in the request body",
+            };
+            return;
+        }
+
+        const entity = {
+            PartitionKey: { _: product },
+            RowKey: { _: new Date().getTime().toString() },
+            title: { _: title },
+            content: { _: content },
+        };
+
+        const result = await insertEntity("Products", entity);
+
+        context.res = {
+            status: 200,
+            body: result,
+        };
+
+    } catch (error) {
+        context.res = {
+            status: 500,
+            body: `Request error. ${error.message}`,
+        };
+    }
 }
